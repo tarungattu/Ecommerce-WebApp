70fc6739dbd95556e0f5d7fa169ae57e22904df0
Tarun Gattu <162986890+tarungattu@users.noreply.github.com>
2025-10-17 16:46:20 -0400

    create product entity and initialize h2 db

--- server/e-commerce-project/e-commerce-project/src/main/java/com/tarungattu/e_commerce_project/controllers/ProductController.java
+++ server/e-commerce-project/e-commerce-project/src/main/java/com/tarungattu/e_commerce_project/controllers/ProductController.java
@@ -1,4 +1,14 @@
 package com.tarungattu.e_commerce_project.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class ProductController {
+
+    @RequestMapping("/")
+    public String test(){
+        return "Working";
+    }
 }

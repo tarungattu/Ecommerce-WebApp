51868ee87bb00c76358b26678ea1b504fa8b4ca5
Tarun Gattu <162986890+tarungattu@users.noreply.github.com>
2025-10-23 11:21:58 -0400

    Add JPQL search feature for products

--- server/e-commerce-project/e-commerce-project/src/main/java/com/tarungattu/e_commerce_project/controllers/ProductController.java
+++ server/e-commerce-project/e-commerce-project/src/main/java/com/tarungattu/e_commerce_project/controllers/ProductController.java
@@ -94,4 +94,10 @@ public class ProductController {
             return new ResponseEntity<>("Product not found", HttpStatus.NOT_FOUND);
         }
     }
+
+    @GetMapping("/products/search")
+    public ResponseEntity<List<Product>> searchProducts(@RequestParam String keyword){
+        List<Product> products = productService.searchProducts(keyword);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
 }
--- server/e-commerce-project/e-commerce-project/src/main/java/com/tarungattu/e_commerce_project/services/ProductService.java
+++ server/e-commerce-project/e-commerce-project/src/main/java/com/tarungattu/e_commerce_project/services/ProductService.java
@@ -42,4 +42,8 @@ public class ProductService {
     public void deleteProduct(int id) {
         productRepo.deleteById(id);
     }
+
+    public List<Product> searchProducts(String keyword) {
+        return productRepo.searchProducts(keyword);
+    }
 }
